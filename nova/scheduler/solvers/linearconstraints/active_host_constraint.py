004ec46ce70caaf30d15bf291d6bf630af3451f7
xyhuang <huangxy0101@gmail.com>
2014-01-15 16:37:45 -0800

    Deleted unnecessary imports from active_host_scheduler

--- nova/scheduler/solvers/linearconstraints/active_host_constraint.py
+++ nova/scheduler/solvers/linearconstraints/active_host_constraint.py
@@ -17,9 +17,8 @@ from oslo.config import cfg
 
 from nova.openstack.common.gettextutils import _
 from nova.openstack.common import log as logging
-from nova.scheduler import filters
-from nova import servicegroup
 from nova.scheduler.solvers import linearconstraints
+from nova import servicegroup
 
 CONF = cfg.CONF
 
