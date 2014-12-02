79e80a82ac7775371e76ae723101b4c242885232
xyhuang <xinyuahu@cisco.com>
2014-12-03 01:35:49 +0800

    add separated host-manager for nova solver scheduler and modify the previous solver-scheduler accordingly.
    Change-Id: I353c1996c38853f961706a8d2c41cff3815f7d43

--- nova/scheduler/solver_scheduler.py
+++ nova/scheduler/solver_scheduler.py
@@ -87,7 +87,6 @@ class ConstraintSolverScheduler(filter_scheduler.FilterScheduler):
                                          update_group_hosts,
                                          instance_uuids)
 
-
     def _get_final_host_list(self, context, request_spec, filter_properties,
                   instance_properties, update_group_hosts=False,
                   instance_uuids=None):
@@ -99,7 +98,7 @@ class ConstraintSolverScheduler(filter_scheduler.FilterScheduler):
         # this returns a host iterator
         hosts = self._get_all_host_states(context)
         selected_hosts = []
-        hosts = self._get_hosts_stripping_ignored_and_forced(
+        hosts = self.host_manager.get_hosts_stripping_ignored_and_forced(
                                       hosts, filter_properties)
         force_hosts = filter_properties.get('force_hosts', [])
         force_nodes = filter_properties.get('force_nodes', [])
@@ -125,73 +124,3 @@ class ConstraintSolverScheduler(filter_scheduler.FilterScheduler):
             if update_group_hosts is True:
                 filter_properties['group_hosts'].append(chosen_host.obj.host)
         return selected_hosts
-
-    def _get_hosts_stripping_ignored_and_forced(self, hosts,
-            filter_properties):
-        """Filter hosts by stripping any ignored hosts and
-           matching any forced hosts or nodes.
-        """
-
-        def _strip_ignore_hosts(host_map, hosts_to_ignore):
-            ignored_hosts = []
-            for host in hosts_to_ignore:
-                for (hostname, nodename) in host_map.keys():
-                    if host == hostname:
-                        del host_map[(hostname, nodename)]
-                        ignored_hosts.append(host)
-            ignored_hosts_str = ', '.join(ignored_hosts)
-            msg = _('Host filter ignoring hosts: %s')
-            LOG.audit(msg % ignored_hosts_str)
-
-        def _match_forced_hosts(host_map, hosts_to_force):
-            forced_hosts = []
-            for (hostname, nodename) in host_map.keys():
-                if hostname not in hosts_to_force:
-                    del host_map[(hostname, nodename)]
-                else:
-                    forced_hosts.append(hostname)
-            if host_map:
-                forced_hosts_str = ', '.join(forced_hosts)
-                msg = _('Host filter forcing available hosts to %s')
-            else:
-                forced_hosts_str = ', '.join(hosts_to_force)
-                msg = _("No hosts matched due to not matching "
-                        "'force_hosts' value of '%s'")
-            LOG.audit(msg % forced_hosts_str)
-
-        def _match_forced_nodes(host_map, nodes_to_force):
-            forced_nodes = []
-            for (hostname, nodename) in host_map.keys():
-                if nodename not in nodes_to_force:
-                    del host_map[(hostname, nodename)]
-                else:
-                    forced_nodes.append(nodename)
-            if host_map:
-                forced_nodes_str = ', '.join(forced_nodes)
-                msg = _('Host filter forcing available nodes to %s')
-            else:
-                forced_nodes_str = ', '.join(nodes_to_force)
-                msg = _("No nodes matched due to not matching "
-                        "'force_nodes' value of '%s'")
-            LOG.audit(msg % forced_nodes_str)
-
-        ignore_hosts = filter_properties.get('ignore_hosts', [])
-        force_hosts = filter_properties.get('force_hosts', [])
-        force_nodes = filter_properties.get('force_nodes', [])
-
-        if ignore_hosts or force_hosts or force_nodes:
-            # NOTE(deva): we can't assume "host" is unique because
-            #             one host may have many nodes.
-            name_to_cls_map = dict([((x.host, x.nodename), x) for x in hosts])
-            if ignore_hosts:
-                _strip_ignore_hosts(name_to_cls_map, ignore_hosts)
-                if not name_to_cls_map:
-                    return []
-            # NOTE(deva): allow force_hosts and force_nodes independently
-            if force_hosts:
-                _match_forced_hosts(name_to_cls_map, force_hosts)
-            if force_nodes:
-                _match_forced_nodes(name_to_cls_map, force_nodes)
-            hosts = name_to_cls_map.itervalues()
-
-        return hosts
